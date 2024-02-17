f4c2b3701e5989e18bdf6a71ffba1cc0cc1e31ee
iBodik <seveliukbohdan@gmail.com>
2024-02-17 12:42:00 -0500

    i have worked on planet 2

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -4,5 +4,10 @@ public class SolarSystem {
         solar.Stars="100";
         solar.Moon="4";
         solar.Planets="2";
+
+        Planet2 p2 = new Planet2();
+        p2.color="Black";
+        p2.size="54511151";
+        p2.temp="5454454";
     }
 }
